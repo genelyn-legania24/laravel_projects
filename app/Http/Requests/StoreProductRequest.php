55f70bea2c7d7e7ca8b47b168b4a86659722bdfb
jsp6a18 <jsp6a18@scs.local>
2025-04-22 23:23:49 -0700

    third

---
+++ app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+class StoreProductRequest extends FormRequest
+{
+ /**
+ * Determine if the user is authorized to make this request.
+ */
+ public function authorize(): bool
+ {
+ return true;
+ }
+
+ public function rules(): array
+ {
+ return [
+ 'code' => 'required|string|max:50|unique:products,code',
+ 'name' => 'required|string|max:250',
+ 'quantity' => 'required|integer|min:1|max:10000',
+ 'price' => 'required',
+ 'description' => 'nullable|string'
+ ];
+ }
+}
